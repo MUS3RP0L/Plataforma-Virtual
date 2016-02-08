5fd13a4f45592bbafc611ef4e8fb1295fe646f67
erickguis <aleguis@icloud.com>
2016-02-08 18:25:28 -0400

    installed debugbar

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         */
         Collective\Html\HtmlServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
 
     ],
@@ -208,6 +209,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
