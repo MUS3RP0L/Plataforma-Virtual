8daa340fbb5bea0a9cb9dfcb7644b5bf4baf9a0a
erickguis <aleguis@icloud.com>
2016-02-19 19:32:16 -0400

    working on validations to import

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -5,6 +5,7 @@ namespace Muserpol\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Session;
+use Validator;
 use Muserpol\Http\Requests;
 use Muserpol\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
@@ -48,13 +49,21 @@ class ImportController extends Controller
 
 			if ($count < count($col))
 			{
-				$message = "Falta Columnas Favor Verificar el Archivo";
+				$message = "Falta Columnas, favor Verificar el Archivo";
 				Session::flash('message', $message);
 				return view('import.import_select');
+				break;
 			}
 		});
+		$col = array('car', 'pat', 'mat', 'nom', 'nom2', 'apes', 'eciv', 'sex', 'nac', 'ing', 'mes', 'a_o', 'uni', 'desg', 
+						'niv', 'gra', 'item', 'sue', 'cat', 'est', 'carg', 'fro', 'ori', 'bseg', 'dfu', 'nat', 'lac', 'pre', 'sub', 'gan', 'mus');
+
 
      	Excel::selectSheetsByIndex(0)->filter('chunk')->select($col)->load($filename,$reader)->chunk(500, function($results) {
+
+     					$col = array('car', 'pat', 'mat', 'nom', 'nom2', 'apes', 'eciv', 'sex', 'nac', 'ing', 'mes', 'a_o', 'uni', 'desg', 
+						'niv', 'gra', 'item', 'sue', 'cat', 'est', 'carg', 'fro', 'ori', 'bseg', 'dfu', 'nat', 'lac', 'pre', 'sub', 'gan', 'mus');
+
 
      		global $countAfi, $countApor;
 
@@ -62,6 +71,13 @@ class ImportController extends Controller
 				
 				set_time_limit(36000);
 
+				if(!$result->car)
+				{
+					$message = "Carnet Vacío Solo se importaron " . $countAfi . " Afiliados y " . $countApor.  " Registros de Planilla" ;
+					Session::flash('message', $message);
+					return view('import.import_select');
+					break;
+				}
 
 				$carnet = Util::zero($result->car);
 
@@ -69,66 +85,82 @@ class ImportController extends Controller
 				
 				if ($afiliado === null) {
 
-					$afiliado = new Afiliado;
-	        		$afiliado->ci = $carnet;
-	        		$afiliado->pat = $result->pat;
-	        		$afiliado->mat = $result->mat;
-	        		$afiliado->nom = $result->nom;
-	        		$afiliado->nom2 = $result->nom2;
-	        		$afiliado->ap_esp = $result->apes;
-	        		$afiliado->est_civ = $result->eciv;
-	        		$afiliado->sex = $result->sex;
-	        		$afiliado->matri = Util::calcMatri($result->nac, $afiliado->pat, $afiliado->mat, $afiliado->nom, $afiliado->sex);
-	        		$afiliado->fech_nac = Util::date($result->nac);
-	        		$afiliado->fech_ing = Util::date($result->ing);
-	       	 		$afiliado->save();
-	       	 		$countAfi ++;
-
+		            $rules = [
+		                'car' => 'regex:/^[a-záéíóúàèìòùäëïöüñ\s]+$/i',
+		            ];
+		            $messages = [
+            			'ape.required' => 'El campo apellidos tiene que ser solo letras',
+        			];
+        			
+        			$validator = Validator::make((array)$result, $rules, $messages);
+        
+			        if ($validator->fails()){
+			            return redirect('import.import_select')
+			            ->withErrors($validator)
+			            ->withInput();
+			        }
+			        else
+			       	{	       		
+						$afiliado = new Afiliado;
+		        		$afiliado->ci = $carnet;
+		        		$afiliado->pat = $result->pat;
+		        		$afiliado->mat = $result->mat;
+		        		$afiliado->nom = $result->nom;
+		        		$afiliado->nom2 = $result->nom2;
+		        		$afiliado->ap_esp = $result->apes;
+		        		$afiliado->est_civ = $result->eciv;
+		        		$afiliado->sex = $result->sex;
+		        		$afiliado->matri = Util::calcMatri($result->nac, $afiliado->pat, $afiliado->mat, $afiliado->nom, $afiliado->sex);
+		        		$afiliado->fech_nac = Util::date($result->nac);
+		        		$afiliado->fech_ing = Util::date($result->ing);
+		       	 		$afiliado->save();
+		       	 		$countAfi ++;
+			       	}
 				}
 
-				$aporte = Aporte::where('mes', '=', $result->mes)
-								->where('anio', '=', $result->a_o)
-								->where('afiliado_id', '=', $afiliado->id)->first();
+				// $aporte = Aporte::where('mes', '=', $result->mes)
+				// 				->where('anio', '=', $result->a_o)
+				// 				->where('afiliado_id', '=', $afiliado->id)->first();
 
-				if ($aporte === null) {
+				// if ($aporte === null) {
 
-					$aporte = new Aporte;
-					$aporte->afiliado_id = $afiliado->id;
-					$aporte->mes = $result->mes;
-					$aporte->anio = $result->a_o;
-					$aporte->uni = $result->uni;
-					$aporte->desg = $result->desg;
-					$aporte->niv = $result->niv;
-					$aporte->gra = $result->gra;
-					$aporte->item = $result->item;
+				// 	$aporte = new Aporte;
+				// 	$aporte->afiliado_id = $afiliado->id;
+				// 	$aporte->mes = $result->mes;
+				// 	$aporte->anio = $result->a_o;
+				// 	$aporte->uni = $result->uni;
+				// 	$aporte->desg = $result->desg;
+				// 	$aporte->niv = $result->niv;
+				// 	$aporte->gra = $result->gra;
+				// 	$aporte->item = $result->item;
 
-					$aporte->sue = Util::decimal($result->sue);
-					$aporte->b_ant = Util::decimal($result->cat);
-					$aporte->cat = Util::calcCat($aporte->b_ant,$aporte->sue);
-					$aporte->b_est = Util::decimal($result->est);
-					$aporte->b_car = Util::decimal($result->carg);
-					$aporte->b_fro = Util::decimal($result->fro);
-					$aporte->b_ori = Util::decimal($result->ori);
-					$aporte->b_seg = Util::decimal($result->bseg);
-					$aporte->dfu = $result->dfu;
-					$aporte->nat = $result->nat;
-					$aporte->lac = $result->lac;
-					$aporte->pre = $result->pre;
-					$aporte->sub = Util::decimal($result->sub);
-					$aporte->gan = Util::decimal($result->gan);
-					$aporte->cot = (FLOAT)$aporte->sue + (FLOAT)$aporte->b_ant + (FLOAT)$aporte->b_est + (FLOAT)$aporte->b_car + (FLOAT)$aporte->b_fro + (FLOAT)$aporte->b_ori;
-					// $aporte->cot_adi = ;
-					$aporte->mus = Util::decimal($result->mus);
-	     			$aporte->save();
-	     			$countApor ++;
-	     		}
+				// 	$aporte->sue = Util::decimal($result->sue);
+				// 	$aporte->b_ant = Util::decimal($result->cat);
+				// 	$aporte->cat = Util::calcCat($aporte->b_ant,$aporte->sue);
+				// 	$aporte->b_est = Util::decimal($result->est);
+				// 	$aporte->b_car = Util::decimal($result->carg);
+				// 	$aporte->b_fro = Util::decimal($result->fro);
+				// 	$aporte->b_ori = Util::decimal($result->ori);
+				// 	$aporte->b_seg = Util::decimal($result->bseg);
+				// 	$aporte->dfu = $result->dfu;
+				// 	$aporte->nat = $result->nat;
+				// 	$aporte->lac = $result->lac;
+				// 	$aporte->pre = $result->pre;
+				// 	$aporte->sub = Util::decimal($result->sub);
+				// 	$aporte->gan = Util::decimal($result->gan);
+				// 	$aporte->cot = (FLOAT)$aporte->sue + (FLOAT)$aporte->b_ant + (FLOAT)$aporte->b_est + (FLOAT)$aporte->b_car + (FLOAT)$aporte->b_fro + (FLOAT)$aporte->b_ori;
+				// 	// $aporte->cot_adi = ;
+				// 	$aporte->mus = Util::decimal($result->mus);
+	   //   			$aporte->save();
+	   //   			$countApor ++;
+	   //   		}
       		}
 
 		});
 
-      	$message = "Se realizaron " . $countAfi . " registros de Afiliados Nuevos y " . $countApor . " en Planillas";
+      	// $message = "Se realizaron " . $countAfi . " registros de Afiliados Nuevos y " . $countApor . " en Planillas";
 
-        Session::flash('message', $message);
+       //  Session::flash('message', $message);
 
 		return view('import.import_select');
     }
