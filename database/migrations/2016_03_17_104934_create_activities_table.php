338ed7be0b7d6e4f979c444a464f612788ee8269
erickguis <aleguis@icloud.com>
2016-03-17 14:50:05 -0400

    working on activities

--- database/migrations/2016_03_17_104934_create_activities_table.php
+++ database/migrations/2016_03_17_104934_create_activities_table.php
@@ -13,10 +13,27 @@ class CreateActivitiesTable extends Migration
     public function up()
     {
         Schema::create('activities', function (Blueprint $table) {
+
             $table->increments('id');
             $table->timestamps();
+            $table->UnsignedBigInteger('afiliado_id');
+            $table->UnsignedBigInteger('user_id');
+
+            $table->UnsignedBigInteger('aporte_id');
+            $table->UnsignedBigInteger('pago_id');
+            $table->UnsignedBigInteger('note_id');
+            $table->UnsignedBigInteger('titular_id');
+            $table->UnsignedBigInteger('ipc_tasa_id');
+            $table->UnsignedBigInteger('apor_tasa_id');
+            $table->UnsignedBigInteger('sueldo_id');
+            $table->text('message');
+            $table->integer('activity_type_id');
+            
+            $table->foreign('user_id')->references('id')->on('users'); 
+
         });
     }
+
 
     /**
      * Reverse the migrations.
