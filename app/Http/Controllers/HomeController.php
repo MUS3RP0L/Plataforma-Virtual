7e6f46c82315fa6b87918c82f25d418f5b9a911d
erickguis <aleguis@icloud.com>
2016-03-31 16:21:37 -0400

    working on dashboard - backend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,16 +37,10 @@ class HomeController extends Controller
 
 	public function showIndex()
 	{
-		// $anios = DB::table('aportes')->select(DB::raw('DISTINCT (aportes.anio ) anio'))->get();
-		
-  //       $i=0; $a = $request->anio;
-  //       foreach ($anios as $item) {
-  //           $anio = $item->anio;
-  //           if($i==$a)
-  //           {break;}
-  //           $i++;
-  //       }
 
+      $select = DB::raw('COUNT (DISTINCT afiliados.id) totalAfiServ')
+      $totalAfiServ = DB::table('afiliados')
+                    ->select($select)
   //       $totalRegistros = DB::table('aportes')
   //               ->select(DB::raw('COUNT(*) registros, SUM(aportes.sue) sueldo, SUM(aportes.b_ant) anti,
   //                               SUM(aportes.b_est) b_est, SUM(aportes.b_car) b_car,
@@ -68,31 +62,12 @@ class HomeController extends Controller
   //           $mus = $item->mus;
   //       }
 
-  //       $anios = array('9' => '19','0' => '20','1' => '20','2' => '20','3' => '20');
-  //       $a = substr($anio, 0, 1);
-  //       $meses = array('01' => 'Enero','02' => 'Febrero','03' => 'Marzo','04' => 'Abril','05' => 'Mayo','06' => 'Junio','07' => 'Julio','08' => 'Agosto','09' => 'Septiembre','10' => 'Octubre','11' => 'Noviembre','12' => 'Diciembre');
-
-  //       $data = array(
-  //           'totalRegistros' => $registros,
-  //           'totalSueldo' => Util::formatMoney($sueldo),
-  //           'totalAnti' => Util::formatMoney($anti),
-  //           'totalB_est' => Util::formatMoney($b_est),
-  //           'totalB_car' => Util::formatMoney($b_car),
-  //           'totalB_fro' => Util::formatMoney($b_fro),
-  //           'totalB_ori' => Util::formatMoney($b_ori),
-  //           'totalB_seg' => Util::formatMoney($b_seg),
-  //           'totalGanado' => Util::formatMoney($gan),
-  //           'totalMuserpol' => Util::formatMoney($mus),
-  //           'anio' => $anios[$a] . $anio,
-  //           'mes' => $meses[$request->mes]
-  //       );
-
-		// return view('home', $data);
-
     $activities = Activity::orderBy('created_at', 'desc')->take(10)->get();
     
     $data = [
       'activities' => $activities,
+      // 'totalRegistros' => $registros,
+      // 'totalSueldo' => Util::formatMoney($sueldo),
     ];
 
     return view('home', $data);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,54 +3,64 @@
 @section('content')
 <div class="container-fluid">
 
+	<div class="col-md-10 col-md-offset-1">
 
-	<div class="row" style="margin: 1% 0 0 0;"> 
-               
-        <div class="col-md-6">
-            <h3>Inicio</h3>
-        </div>
-    </div>
+		<div class="row">  
+	        <div class="col-md-6">
+	            <h3>Inicio</h3>
+	        </div>
+	    </div>
 
-	<div class="row" style="margin: 1% 0 0 0;">
-		<div class="col-md-8">
-		 	<div class="panel panel-primary">
-		        <div class="panel-heading">
-		            <div class="row">  
-		                <div class="col-md-11">
-		                    <h3 class="panel-title">Total de Afiliados</h3>
-		                </div>
-		                <div class="col-md-1 text-right" data-toggle="tooltip" data-placement="top" data-original-title="Editar">
-		                    <div data-toggle="modal" data-target="#myModal-personal"> 
-		                        <span class="glyphicon glyphicon-pencil"  aria-hidden="true"></span>
-		                    </div>
-		                </div>
-		            </div>
-		        </div>
-		        <div class="panel-body" style="font-size: 14px">
-					totales
-		        </div>
-		    </div>
-		</div>
-	</div>
-
-  	<div class="row" style="margin: 1% 0 0 0;">
-		<div class="col-md-8">
-			<div class="panel panel-primary">
-				<div class="panel-heading">Reporte</div>
-
-				<div class="panel-body">
-					<ul class="panel-body list-group">
-				      @foreach ($activities as $activity)
-				        <li class="list-group-item">
-				          <span style="color:#888;font-style:italic">{!! $activity->created_at !!}:</span>
-				          {!! $activity->message !!}
-				        </li>
-				      @endforeach
-				      </ul>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default">
+				  <div class="panel-body" style="font-size: 26px;">
+				    <div>
+         				Afiliados En servicio<br>
+         				
+        			</div>
+				  </div>
 				</div>
 			</div>
 		</div>
-	</div>
 
+	  	<div class="row">
+			<div class="col-md-8">
+					<div class="panel panel-primary">
+						<div class="panel-heading">Reporte</div>
+
+						<div class="panel-body">
+							<ul class="panel-body list-group">
+						      @foreach ($activities as $activity)
+						        <li class="list-group-item">
+						          <span style="color:#888;font-style:italic">{!! $activity->created_at !!}:</span>
+						          {!! $activity->message !!}
+						        </li>
+						      @endforeach
+						      </ul>
+						</div>
+					</div>
+				</div>
+			<div class="col-md-4">
+			 	<div class="panel panel-primary">
+			        <div class="panel-heading">
+			            <div class="row">  
+			                <div class="col-md-11">
+			                    <h3 class="panel-title">Total de Afiliados</h3>
+			                </div>
+			                <div class="col-md-1 text-right" data-toggle="tooltip" data-placement="top" data-original-title="Editar">
+			                    <div data-toggle="modal" data-target="#myModal-personal"> 
+			                        <span class="glyphicon glyphicon-pencil"  aria-hidden="true"></span>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			        <div class="panel-body" style="font-size: 14px">
+						totales
+			        </div>
+			    </div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
