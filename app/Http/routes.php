0b9c784a6af195c84fec3817f39817e7082dc78c
erickguis <aleguis@icloud.com>
2016-04-01 09:37:42 -0400

    working on reports

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -81,6 +81,9 @@ Route::group(['middleware' => 'auth'], function() {
 	//  Totales
 	Route::get('totales', 'ReporteController@ReportAporte');
 	Route::post('ir_totales', 'ReporteController@GenerateReportAporte');
+	
+	Route::get('total_month', 'ReporteController@ReportAporteMonth');
+	Route::post('go_total_month', 'ReporteController@GenerateReportAporteMonth');
 
 
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
 	  	<div class="row">
 			<div class="col-md-8">
 					<div class="panel panel-primary">
-						<div class="panel-heading">Reporte</div>
+						<div class="panel-heading">Actividades</div>
 
 						<div class="panel-body">
 							<ul class="panel-body list-group">
@@ -51,25 +51,7 @@
 						</div>
 					</div>
 				</div>
-			<div class="col-md-4">
-			 	<div class="panel panel-primary">
-			        <div class="panel-heading">
-			            <div class="row">  
-			                <div class="col-md-11">
-			                    <h3 class="panel-title">Total de Afiliados</h3>
-			                </div>
-			                <div class="col-md-1 text-right" data-toggle="tooltip" data-placement="top" data-original-title="Editar">
-			                    <div data-toggle="modal" data-target="#myModal-personal"> 
-			                        <span class="glyphicon glyphicon-pencil"  aria-hidden="true"></span>
-			                    </div>
-			                </div>
-			            </div>
-			        </div>
-			        <div class="panel-body" style="font-size: 14px">
-						totales
-			        </div>
-			    </div>
-			</div>
+			
 		</div>
 	</div>
 </div>
