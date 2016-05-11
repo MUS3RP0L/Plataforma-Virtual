f9a9fd7c0f29cf73fcce88970d8404f42ada467c
erickguis <aleguis@icloud.com>
2016-05-11 11:51:47 -0400

    added breadcrumbs

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -1,10 +1,29 @@
 <?php
 
-// Home
+// Inicio
 Breadcrumbs::register('home', function($breadcrumbs) {
-     $breadcrumbs->push('Inicio', route('home'));
+    $breadcrumbs->push('Inicio', route('home'));
 });
 
+// Afiliado Search
 Breadcrumbs::register('afiliado', function($breadcrumbs) {
-     $breadcrumbs->push('Afiliados', URL::to('afiliado'));
+    $breadcrumbs->push('Afiliados', URL::to('afiliado'));
+});
+
+// Show Afiliado
+Breadcrumbs::register('show_afiliado', function($breadcrumbs, $afiliado) {
+    $breadcrumbs->parent('afiliado');
+    $breadcrumbs->push($afiliado->getTittleName(), URL::to('afiliado/'.$afiliado->id));
+});
+
+// Show Aportes Afiliado
+Breadcrumbs::register('aportes_afiliado', function($breadcrumbs, $afiliado) {
+    $breadcrumbs->parent('show_afiliado', $afiliado);
+    $breadcrumbs->push('Aportes');
+});
+
+// Show Aportes Afiliado
+Breadcrumbs::register('calif_sv_afiliado', function($breadcrumbs, $afiliado) {
+    $breadcrumbs->parent('show_afiliado', $afiliado);
+    $breadcrumbs->push('Calificación Fondo de Retiro');
 });
