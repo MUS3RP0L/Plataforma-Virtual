21e08bd5d727c68519e49857ded0f2e3e47d09f8
erickguis <aleguis@icloud.com>
2016-05-23 10:57:55 -0400

    bug fixes

--- database/seeds/GradoTableSeeder.php
+++ database/seeds/GradoTableSeeder.php
@@ -23,14 +23,14 @@ class GradoTableSeeder extends Seeder
         $statuses = [
             ['id' => '1', 'niv' => '00', 'grad' => '00', 'lit' => 'COMANDANTE GRAL', 'abre' => 'CMTE. GRAL.'],
             ['id' => '2', 'niv' => '00', 'grad' => '01', 'lit' => 'COMANDANTE GRAL', 'abre' => 'CMTE. GRAL.'],
-            ['id' => '3', 'niv' => '00', 'grad' => '02', 'lit' => 'SUBCOMANDANTE', 'abre' => 'CMTE. GRAL.'],
+            ['id' => '3', 'niv' => '00', 'grad' => '02', 'lit' => 'SUBCOMANDANTE', 'abre' => 'SBCMTE. GRAL.'],
             ['id' => '4', 'niv' => '00', 'grad' => '03', 'lit' => 'INSPECTOR GENERAL', 'abre' => 'INSP. GRAL.'],
             ['id' => '5', 'niv' => '00', 'grad' => '04', 'lit' => 'DIRECTOR GENERAL', 'abre' => 'DIR. GRAL.'],
             ['id' => '6', 'niv' => '01', 'grad' => '01', 'lit' => 'CORONEL CON SUELDO DE GENERAL', 'abre' => 'CNL.'],
             ['id' => '7', 'niv' => '01', 'grad' => '02', 'lit' => 'CORONEL', 'abre' => 'CNL.'],      
             ['id' => '8', 'niv' => '01', 'grad' => '03', 'lit' => 'TENIENTE CORONEL', 'abre' => 'TCNL.'],
             ['id' => '9', 'niv' => '01', 'grad' => '04', 'lit' => 'MAYOR', 'abre' => 'MY.'],
-            ['id' => '10', 'niv' => '01', 'grad' => '05', 'lit' => 'CAPITAN', 'abre' => 'GRAL.'],
+            ['id' => '10', 'niv' => '01', 'grad' => '05', 'lit' => 'CAPITAN', 'abre' => 'CAP.'],
             ['id' => '11', 'niv' => '01', 'grad' => '06', 'lit' => 'TENIENTE', 'abre' => 'TTE.'],
             ['id' => '12', 'niv' => '01', 'grad' => '07', 'lit' => 'SUBTENIENTE', 'abre' => 'SBTTE.'],
 
