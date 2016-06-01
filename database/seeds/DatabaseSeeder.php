caddd0b154536d33091e90ab93807ffeaecfe339
erickguis <aleguis@icloud.com>
2016-06-01 16:53:38 -0400

    working on user, upgarde controller

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(RolesTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(GradoTableSeeder::class);   
         $this->call(DepartamentoTableSeeder::class);
