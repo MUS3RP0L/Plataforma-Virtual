28d40409ed73ca7e2a682d97a56e57d79ac97a1f
erickguis <aleguis@icloud.com>
2016-06-10 10:59:13 -0400

    bug fixes

--- app/StateType.php
+++ app/StateType.php
@@ -5,7 +5,7 @@ namespace Muserpol;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class AfiState extends Model
+class StateType extends Model
 {
     protected $table = 'state_types';
 
