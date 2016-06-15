7c10e2a7699b370588d29d025aa7503a4b43bec7
erickguis <aleguis@icloud.com>
2016-06-15 12:02:41 -0400

    created layout print

---
+++ resources/views/print/layoutPrint.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Ventanilla</title>
+    <link rel="stylesheet" href="assets/css/style.css" media="all" />
+  </head>
+  <body>
+    <header class="clearfix">
+      <table class="tableh">
+            <tr>
+              <th style="width: 25%;border: 0px;">
+                <div id="logo">
+                  <img src="assets/images/logo.jpg">
+                </div>
+              </th>
+              <th style="width: 50%;border: 0px">
+                <h3><b>MUTUAL DE SERVICIOS AL POLICÍA<br>
+                    DIRECCIÓN DE BENEFICIOS ECONÓMICOS<br>
+                    UNIDAD DE FONDO DE RETIRO POLICIAL INDIVIDUAL<br>
+                    GESTIÓN {!! $dateY !!}</b></h3>
+              </th>
+              <th style="width: 25%;border: 0px">
+                <div id="logo2">
+                  <img src="assets/images/escudo.jpg">
+                </div>
+              </th>
+            </tr>
+      </table>
+
+      @yield('content')
+
+    </header>
+    <footer>
+      PLATAFORMA VIRUTAL - MUTUAL DE SERVICIOS AL POLICIA
+    </footer>
+  </body>
+</html>
