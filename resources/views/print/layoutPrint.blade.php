0c6736bc10bcc8e67746cd70d6dd81f40dcca347
erickguis <aleguis@icloud.com>
2016-06-16 15:28:56 -0400

    completed print ventanilla tramite fr

--- resources/views/print/layoutPrint.blade.php
+++ resources/views/print/layoutPrint.blade.php
@@ -18,7 +18,8 @@
                 <h3><b>MUTUAL DE SERVICIOS AL POLICÍA<br>
                     DIRECCIÓN DE BENEFICIOS ECONÓMICOS<br>
                     UNIDAD DE FONDO DE RETIRO POLICIAL INDIVIDUAL<br>
-                    GESTIÓN {!! $dateY !!}</b></h3>
+                    @yield('title')
+                    </b></h3>
               </th>
               <th style="width: 25%;border: 0px">
                 <div id="logo2">
--- resources/views/print/ventanilla/show.blade.php
+++ resources/views/print/ventanilla/show.blade.php
@@ -1,13 +1,14 @@
 @extends('print.layoutPrint')
-@section('content')
 
-      <h1>
-        <b>
-          VENTANILLA DE ATENCIÓN AL AFILIADO<br>
-          FONDO DE RETIRO POLICIAL INDIVIDUAL<br>
-          MODALIDAD: {!! $fondoTramite->modalidad->name !!}
-        </b>
-      </h1>
+@section('title')
+<h1>
+  <b>
+  VENTANILLA DE ATENCIÓN AL AFILIADO<br>
+  </b>
+</h1>
+@endsection
+
+@section('content')
       
       <table class="tablet">
         <tr>
@@ -18,8 +19,21 @@
             <div class="title"><b>Fecha de Emisión: La Paz, {!! $date !!}</b></div>
           </td>
         </tr>
+        <tr>
+            <td  colspan="2" style="border: 0px;text-align:right;">
+             <div class="title">Usuario: {{ Auth::user()->ape }} {{ Auth::user()->nom }}</div>
+            </td>
+        </tr>
       </table>
 
+      <h1>
+        <b>
+          
+          FONDO DE RETIRO POLICIAL INDIVIDUAL<br>
+          MODALIDAD: {!! $fondoTramite->modalidad->name !!}
+        </b>
+      </h1>
+      <br>
       <div id="project">
         <table>
             <tr>
@@ -65,43 +79,43 @@
             </tr>
           </table>
       </div>
+       <br> 
+       <table class="tablet">
+          <tr>
+            <td style="width: 60%;border: 0px;">
+              <div class="title"><b>II. REQUISITOS PRESENTADOS</b></div>
+            </td>
+          </tr>
+        </table>
 
-     <table class="tablet">
-        <tr>
-          <td style="width: 60%;border: 0px;">
-            <div class="title"><b>II. REQUISITOS PRESENTADOS</b></div>
-          </td>
-         
-        </tr>
-      </table>
-
-      <div id="project">
-          <table>
-           <tr>
-              <th class="grand service"><center>NRO</center></th>
-              <th class="grand service"><center>REQUISITO</center></th>
-              <th class="grand service"><center>PRESENTADO</center></th>
-              <th class="grand service"><center>FECHA</center></th>
-            </tr>
-            <?php $i=1; ?>
-             @foreach ($documentos as $item)
-            <tr>
-              <td style="width: 5%";class="info">{!! $i !!}</td>  
-              <td style="width: 90%";class="info">{!! $item->requisito->name !!}</td>
-               @if ($item->est == 1)
-                  <td class="info"><center><img class="circle" src="{!! asset('assets/images/check.png') !!}"  alt="icon"></center></td>
-
-
-               @else
+        <div id="project">
+            <table>
+             <tr>
+                <th class="grand">N°</th>
+                <th class="grand">REQUISITO</th>
+                <th class="grand">ESTADO</th>
+                <th class="grand">FECHA</th>
+              </tr>
+              <?php $i=1;?>
+               @foreach ($documentos as $item)
+              <tr>
+                <td style="width:1%" class="info">{!! $i !!}</td>  
+                <td style="width:80%" class="info">{!! $item->requisito->name !!}</td>
+                @if ($item->est == 1)
+                  <td class="info"><center><img class="circle" src="{!! asset('assets/images/check.png') !!}" style="width:70%" alt="icon"></center></td>
+                @else
                   <td class="info"> </td>
-               @endif
-              <td style="width: 20%";class="info"><center>{!! $item->getDataEdit() !!}</center></td>
-           
-            </tr>
-            <?php $i++; ?>
-            @endforeach
-            
-          </table>
-      </div>
+                @endif
+                <td style="width:20%" class="info"><center>{!! $item->getData_fech_requi() !!}</center></td>
+              </tr>
+              <?php $i++;?>
+              @endforeach         
+            </table>
+        </div>
+        <br>
+        <div id="notices">
+          <div><b>Nota:</b></div>
+          <div class="notice">Una vez presentada la documentación no sera devuelta.</div>
+        </div>
 
 @endsection
