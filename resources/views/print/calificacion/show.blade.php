e4cdc6366cdd3b34df1cb6ea755cc81405e49594
erickguis <aleguis@icloud.com>
2016-06-17 16:16:41 -0400

    working on calificacion tramite fr

--- resources/views/print/calificacion/show.blade.php
+++ resources/views/print/calificacion/show.blade.php
@@ -5,7 +5,8 @@
 @endsection
 
 @section('title2')
-  CERTIFICACIÓN
+  CALIFICACIÓN<br>
+  <h3>(Página 1/2)</h3>
 @endsection
 
 @section('content')
@@ -17,7 +18,7 @@
             </tr>
             <tr>
               <th class="service">NOMBRE DEL BENEFICIARIO</th>
-              <td class="info">{!! $afiliado->getFullNametoPrint() !!}</td>
+              <td class="info" style="width: 60%">{!! $afiliado->getFullNametoPrint() !!}</td>
             </tr>
             <tr>
               <th class="service">CARNET DE IDENTIDAD</th>
@@ -64,7 +65,7 @@
             </tr>
             <tr>
               <th class="service">GRADO</th>
-              <td class="info">{!! $afiliado->grado->lit !!}</td>
+              <td class="info" style="width: 60%">{!! $afiliado->grado->lit !!}</td>
             </tr>
             <tr>
               <th class="service">FECHA DE ALTA</th>
@@ -103,7 +104,7 @@
             </tr>
             <tr>
               <th class="service">NOMBRE DE CONYUGE</th>
-              <td class="info">{!! $conyuge->getFullNametoPrint() !!}</td>
+              <td class="info" style="width: 60%">{!! $conyuge->getFullNametoPrint() !!}</td>
             </tr>
             <tr>
               <th class="service">FECHA DE NACIMIENTO</th>
@@ -126,11 +127,11 @@
       <div id="project">
           <table>
             <tr>
-              <th colspan="2" class="grand service">D) DATOS DE SOLICITANTE</th>
+              <th colspan="2" class="grand service">DATOS DE SOLICITANTE</th>
             </tr>
             <tr>
               <th class="service">NOMBRE DE SOLICITANTE</th>
-              <td class="info">{!! $solicitante->getFullNametoPrint() !!}</td>
+              <td class="info" style="width: 60%">{!! $solicitante->getFullNametoPrint() !!}</td>
             </tr>
             <tr>
               <th class="service">PARENTESCO CON TITULAR</th>
@@ -154,4 +155,117 @@
             </tr>
           </table>
       </div>
+
+      
+
+<div class="page-break"></div>
+
+
+
+    <header class="clearfix">
+        <table class="tableh">
+              <tr>
+                <th style="width: 25%;border: 0px;">
+                  <div id="logo">
+                    <img src="assets/images/logo.jpg">
+                  </div>
+                </th>
+                <th style="width: 50%;border: 0px">
+                  <h3><b>MUTUAL DE SERVICIOS AL POLICÍA<br>
+                      DIRECCIÓN DE BENEFICIOS ECONÓMICOS<br>
+                      UNIDAD DE FONDO DE RETIRO POLICIAL INDIVIDUAL<br>
+                      FICHA TÉCNICA DE CALIFICACIÓN
+                      </b></h3>
+                </th>
+                <th style="width: 25%;border: 0px">
+                  <div id="logo2">
+                    <img src="assets/images/escudo.jpg">
+                  </div>
+                </th>
+              </tr>
+        </table>
+        <table class="tablet">
+          <tr>
+            <td style="border: 0px;">
+              <div class="title"><b>N° de Tramite: {{ $fondoTramite->getNumberTram() }}</b></div>
+            </td>
+            <td style="border: 0px;text-align:right;">
+              <div class="title"><b>Fecha de Emisión: La Paz, {!! $date !!}</b></div>
+            </td>
+          </tr>
+          <tr>
+              <td  colspan="2" style="border: 0px;text-align:right;">
+               <div class="title">Usuario: {{ Auth::user()->ape }} {{ Auth::user()->nom }}</div>
+              </td>
+          </tr>
+        </table>
+        <h1>
+          <b>       
+            FONDO DE RETIRO POLICIAL INDIVIDUAL<br>
+            MODALIDAD: {!! $fondoTramite->modalidad->name !!}<br>
+            CALIFICACIÓN
+            <h3>(Página 2/2)</h3>
+          </b>
+        </h1>
+        <br>
+
+    </header>
+
+      <table class="tablet">
+        <tr>
+          <td style="width: 60%;border: 0px;">
+            <div class="title"><b>II. INFORMACIÓN TÉCNICA</b></div>
+          </td>
+        </tr>
+      </table>
+      <br>
+      <table>
+          <tr>
+            <th colspan="2" class="grand service">A) ESTADO DE LA CUENTA INDIVIDUAL</th>
+          </tr>
+          <tr>
+            <th class="service">PERIODO DE APORTES</th>
+            <td class="total">{!! $fondoTramite->fech_ini_pcot ? "Desde " . $fondoTramite->getFull_fech_ini_pcot() . " - Hasta " . $fondoTramite->getFull_fech_fin_pcot() : '' !!}</td>
+          </tr>       
+          <tr>
+            <th class="service">TIEMPO COTIZABLE</th>
+            <td class="total">{!! $fondoTramite->fech_ini_pcot ? $fondoTramite->getYearsAndMonths_fech_pcot() : '' !!}</td>
+          </tr>
+          <tr>
+            <th class="service">TOTAL DE MESES COTIZABLES</th>
+            <td class="total">{!! $fondoTramite->fech_ini_pcot ? $fondoTramite->getMonths_fech_pcot() : '' !!}</td>
+          </tr>
+      </table>
+
+      <table>
+          <tr>
+            <th colspan="2" class="grand service">B) DATOS ECONÓMICOS DEL AFILIADO</th>
+          </tr>
+          <tr>
+            <th class="service">TOTAL COTIZABLE</th>
+            <td class="total">0.00</td>
+          </tr>
+          <tr>
+            <th class="service">TOTAL COTIZABLE ADICIONAL (ITEM "0")</th>
+            <td class="total">0.00</td>
+          </tr>
+          <tr>
+            <th class="service">TOTAL GENERAL COTIZABLE</th>
+            <td class="grand total">0.00</td>
+          </tr>
+          <tr>
+            <th class="service">TOTAL FONDO DE RETIRO (1,85%)</th>
+            <td class="total">0.00</td>
+          </tr>
+          <tr>
+            <th class="service">RENDIMIENTO OBTENIDO</th>
+            <td class="total">0.00</td>
+          </tr>
+          <tr>
+            <th class="service">TOTAL FONDO DE RETIRO</th>
+            <td class="grand total">0.00</td>
+          </tr>
+      </table>
+
+      
 @endsection
