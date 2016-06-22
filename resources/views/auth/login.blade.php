508b6f29e8d419e2a3f82fe6cce7cc4dd4ac2ea6
erickguis <aleguis@icloud.com>
2016-06-22 10:37:18 -0400

    upgrade show login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,52 +2,52 @@
 
 @section('content')
 
-<div class="container-fluid">
-	
-    @if($errors->has())
+<div class="container">
+	@if (Session::has('error'))
         <div class="alert alert-warning" role="alert">
            @foreach ($errors->all() as $error)
-              <div>{{ $error }}</div>
+              <div><h4>{{ $error }}</h4></div>
           @endforeach
+          <div><h4>{{Session::get('error')}}</h4></div>
         </div>
     @endif
+</div>
 
-  	<div class="row" style="margin: 6% 0 0 0;">
-    	<div class="col-md-6 col-md-offset-3">
-      		<div class="panel panel-primary">
-        		<div class="panel-heading"><h4 class="text-center">Plataforma Virtual - Mutual de Servicios al Policía</h4></div>
-        			<div class="panel-body">
+<div class="row" style="margin: 6% 0 0 0;">
+	<div class="col-md-6 col-md-offset-3">
+  		<div class="panel panel-primary">
+    		<div class="panel-heading"><h4 class="text-center">Plataforma Virtual - Mutual de Servicios al Policía</h4></div>
+    			<div class="panel-body">
+		        	{!! Form::open(['url' => 'login', 'role' => 'form', 'class' => 'form-horizontal']) !!}
+			            <br>
+			            <div class="form-group">
+			              	{!! Form::label('usermane', 'Carnet de Identidad', ['class' => 'col-md-5 control-label']) !!}
+			              	<div class="col-md-4">
+                  				{!! Form::text('username', null, ['class' => 'form-control', 'required' => 'required']) !!}
+			                	<span class="help-block">Ingrese su número de carnet</span>
+			              	</div>
+			            </div>
 
-			        	{!! Form::open(['url' => 'login', 'role' => 'form', 'class' => 'form-horizontal']) !!}
-				            <br>
-				            <div class="form-group">
-				              	{!! Form::label('usermane', 'Carnet de Identidad', ['class' => 'col-md-5 control-label']) !!}
-				              	<div class="col-md-4">
-	                  				{!! Form::text('username', null, ['class' => 'form-control', 'required' => 'required']) !!}
-				                	<span class="help-block">Ingrese su número de carnet</span>
-				              	</div>
-				            </div>
-
-				            <div class="form-group">
-				                {!! Form::label('password', 'Contraseña', ['class' => 'col-md-5 control-label']) !!}
-				              	<div class="col-md-4">
-				                	{!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
-				                	<span class="help-block">Ingrese su contraseña</span>
-				              	</div>
-				            </div>
-
-					        <div class="row text-center">
-			                    <div class="form-group">
-			                        <div class="col-md-12">
-			                            <button type="submit" class="btn btn-raised btn-primary">&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-lock" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;</button>
-			                        </div>
-			                    </div>
-			                </div>
-
-			          	{!! Form::close() !!}
-				</div>
+			            <div class="form-group">
+			                {!! Form::label('password', 'Contraseña', ['class' => 'col-md-5 control-label']) !!}
+			              	<div class="col-md-4">
+			                	{!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
+			                	<span class="help-block">Ingrese su contraseña</span>
+			              	</div>
+			            </div>
+						<br>
+				        <div class="row text-center">
+		                    <div class="form-group">
+		                        <div class="col-md-12">
+		                            <button type="submit" class="btn btn-raised btn-primary">&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-lock" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;</button>
+		                        </div>
+		                    </div>
+		                </div>
+		          	{!! Form::close() !!}
+		        </div>
 			</div>
 		</div>
 	</div>
 </div>
+
 @endsection
