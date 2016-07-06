eeb5cf2c3feacaf1b11f06ef977c5f2d94a0e817
erickguis <aleguis@icloud.com>
2016-07-06 17:43:01 -0400

    bug fixes

--- database/migrations/2016_02_29_113942_create_ipcrates_table.php
+++ database/migrations/2016_02_29_113942_create_ipcrates_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIpctasasTable extends Migration
+class CreateIpcRatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -23,7 +23,6 @@ class CreateIpctasasTable extends Migration
             $table->foreign('user_id')->references('id')->on('users'); 
 
         });
-
     }
 
     /**
