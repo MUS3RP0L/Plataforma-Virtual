d674c581ad495fa97fca46a263a5ead475b58e2e
erickguis <aleguis@icloud.com>
2016-07-14 19:07:58 -0400

    update spouse model

--- app/Spouse.php
+++ app/Spouse.php
@@ -11,59 +11,68 @@ class Spouse extends Model
 
     protected $dates = ['deleted_at'];
 
-	protected $fillable = [
-	    
+    protected $fillable = [
+        
         'user_id',
         'affiliate_id',
-		'identity_card',
-		'last_name',
-		'mothers_last_name',
-		'first_name',
-		'second_name',
-		'birth_date',
+        'identity_card',
+        'last_name',
+        'mothers_last_name',
+        'first_name',
+        'second_name',
+        'birth_date',
         'date_death',
-        'reason_death'		
-	];
+        'reason_death'      
+    ];
 
-	protected $guarded = ['id'];
+    protected $guarded = ['id'];
 
-	public function affiliate()
+    public function affiliate()
     {
         return $this->belongsTo('Muserpol\Affiliate');
     }
 
-    public function scopeAfiIs($query, $id)
+    public function scopeAffiliateidIs($query, $id)
     {
         return $query->where('affiliate_id', $id);
     }
 
+    public function getFullDateDeath()
+    {   
+        return Util::getdateabre($this->date_death);
+    }
+
+
+
+
+
+
+
+
     public function getDataEdit_fech_nac()
-    {	
-		if ($this->fech_nac) {
-		return date("d", strtotime($this->fech_nac))."/".date("m", strtotime($this->fech_nac)). "/".date("Y", strtotime($this->fech_nac));
- 		}
+    {   
+        if ($this->fech_nac) {
+        return date("d", strtotime($this->fech_nac))."/".date("m", strtotime($this->fech_nac)). "/".date("Y", strtotime($this->fech_nac));
+        }
     }
 
     public function getDataEdit_fech_dece()
-    {	
-		if ($this->fech_dece) {
-		return date("d", strtotime($this->fech_dece))."/".date("m", strtotime($this->fech_dece)). "/".date("Y", strtotime($this->fech_dece));
- 		}
+    {   
+        if ($this->fech_dece) {
+        return date("d", strtotime($this->fech_dece))."/".date("m", strtotime($this->fech_dece)). "/".date("Y", strtotime($this->fech_dece));
+        }
     }
 
-    public function getFullDateNac()
-    {	
-		$meses = array("ENE","FEB","MAR","ABR","MAY","JUN","JUL","AGO","SEP","OCT","NOV","DIC");
-		if ($this->fech_nac) {
-			return date("d", strtotime($this->fech_nac))." ".$meses[date("n", strtotime($this->fech_nac))-1]. " ".date("Y", strtotime($this->fech_nac));
-		}
-    }public function getFullDateDece()
-    {	
-		$meses = array("ENE","FEB","MAR","ABR","MAY","JUN","JUL","AGO","SEP","OCT","NOV","DIC");
-		if ($this->fech_dece) {
-			return date("d", strtotime($this->fech_dece))." ".$meses[date("n", strtotime($this->fech_dece))-1]. " ".date("Y", strtotime($this->fech_dece));
-		}
+    public function getFullBirthDate()
+    {   
+        return Util::getdateabre($this->birth_date);
     }
+
+
+
+
+
+    
 
     public function getFullNametoPrint()
     {
@@ -82,6 +91,26 @@ class Spouse extends Model
         if ($this->fech_dece) {
             $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
             return date("d", strtotime($this->fech_dece))." ".$meses[date("n", strtotime($this->fech_dece))-1]. " ".date("Y", strtotime($this->fech_dece)); 
+        }
+    }
+
+    public function getFullName()
+    {
+        return $this->pat . ' ' . $this->mat. ' ' . $this->nom. ' ' .$this->nom2;
+    }
+}
+
+Spouse::created(function($spouse)
+{
+    Activity::createdSpouse($spouse);
+});
+
+Spouse::updating(function($spouse)
+{
+    Activity::updateSpouse($spouse);
+
+});
+n", strtotime($this->fech_dece))-1]. " ".date("Y", strtotime($this->fech_dece)); 
         }
     }
 
