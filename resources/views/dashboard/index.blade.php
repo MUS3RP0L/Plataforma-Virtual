35a0f15a19be2aa0ac6e2bb4430a46b54b0b67b8
felixbvf <felixbvf@gmail.com>
2016-07-21 15:41:08 -0400

    Upgrade Dashboard  View index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -82,63 +82,63 @@
 
 <script>
 
-	var doughnutData = [
-				{
-					value: {!! json_encode($Total_AffiliatebyState[1]) !!},
-					color:"#F7464A",
-					highlight: "#FF5A5E",
-					label: "Servicio" //rojo
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[2]) !!},
-					color: "#46BFBD",
-					highlight: "#5AD3D1",
-					label: "Comision" //verde
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[3]) !!},
-					color: "#FDB45C",
-					highlight: "#FFC870",
-					label: "Disponibilidad" //amarillo
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[4]) !!},
-					color: "#949FB1",
-					highlight: "#A8B3C5",
-					label: "Fallecido"  //grey
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[5]) !!},
-					color: "#4D5360",
-					highlight: "#616774",
-					label: "Jubilado"  //Dark Grey
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[6]) !!},
-					color: "#FF8C00",
-					highlight: "#616774",
-					label: "Jublición por Invalidez"  //Dark Orange
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[7]) !!},
-					color: "#8FBC8F",
-					highlight: "#616774",
-					label: "Forzosa" //Dark Sea Green
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[8]) !!},
-					color: "#1E90FF",
-					highlight: "#FFC870",
-					label: "Voluntario" //DodgerBlue
-				},
-				{
-					value: {!! json_encode($Total_AffiliatebyState[9]) !!},
-					color:"#B0C4DE",
-					highlight: "#FF5A5E",
-					label: "Temporal" //LightSteelBlue
-				}
+var doughnutData = [
+			{
+				value: {!! json_encode($Total_AffiliatebyState[1]) !!},
+				color:"#F7464A",
+				highlight: "#FF5A5E",
+				label: "Servicio" //rojo
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[2]) !!},
+				color: "#46BFBD",
+				highlight: "#5AD3D1",
+				label: "Comision" //verde
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[3]) !!},
+				color: "#FDB45C",
+				highlight: "#FFC870",
+				label: "Disponibilidad" //amarillo
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[4]) !!},
+				color: "#949FB1",
+				highlight: "#A8B3C5",
+				label: "Fallecido"  //grey
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[5]) !!},
+				color: "#4D5360",
+				highlight: "#616774",
+				label: "Jubilado"  //Dark Grey
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[6]) !!},
+				color: "#FF8C00",
+				highlight: "#FC9E2B",
+				label: "Jublición por Invalidez"  //Dark Orange
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[7]) !!},
+				color: "#8FBC8F",
+				highlight: "#8FC08F",
+				label: "Forzosa" //Dark Sea Green
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[8]) !!},
+				color: "#1E90FF",
+				highlight: "#3496F8",
+				label: "Voluntario" //DodgerBlue
+			},
+			{
+				value: {!! json_encode($Total_AffiliatebyState[9]) !!},
+				color:"#B0C4DE",
+				highlight: "#BAC9DE",
+				label: "Temporal" //LightSteelBlue
+			}
 
-			];
+		];
 
 	var pieData = [
 				{
@@ -205,32 +205,32 @@
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["ORURO"]) !!},
-					color: "#F0F8FF",
-					highlight: "#616774",
-					label: "Oruro" //AliceBlue
+					color: "#D8BFD8",
+					highlight: "#E1CFE1",
+					label: "Oruro" //Thistle
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["POTOSI"]) !!},
-					color: "#4D5360",
-					highlight: "#616774",
-					label: "Dark Grey"
+					color: "#008080",
+					highlight: "#0E9696",
+					label: "Potosí" // teal
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["TUPIZA"]) !!},
 					color: "#A52A2A",
-					highlight: "#616774",
+					highlight: "#A44444",
 					label: "Tupiza" //Brown
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["VILLAZON"]) !!},
 					color: "#6495ED",
-					highlight: "#616774",
+					highlight: "#759EEB",
 					label: "Villazon" //CornFlowerBlue
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["TARIJA"]) !!},
 					color: "#DC143C",
-					highlight: "#616774",
+					highlight: "#DA3254",
 					label: "Tarija" //Crimson
 				},
 				{
@@ -248,60 +248,59 @@
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["BERMEJO"]) !!},
 					color: "#00BFFF",
-					highlight: "#616774",
+					highlight: "#8F258F",
 					label: "Bermejo" // DeepSkyBlue
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["SANTA CRUZ"]) !!},
 					color: "#1E90FF",
-					highlight: "#616774",
+					highlight: "#2994FF",
 					label: "Santa Cruz" //DodgerBlue
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["SAN MATIAS"]) !!},
-					color: "#4D5360",
-					highlight: "#616774",
-					label: "Dark Grey"
+					color: "#D2B48C",
+					highlight: "#D1B999",
+					label: "San Matias" //tan
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["S. I. VELASCO"]) !!},
 					color: "#228B22",
-					highlight: "#616774",
+					highlight: "#259C25",
 					label: "Velasco" //ForestGreen
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["PTO. SUAREZ"]) !!},
 					color: "#FFFAF0",
-					highlight: "#616774",
+					highlight: "#F6F3EC",
 					label: "Pto. Suarez" //FloralWhite
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["BENI"]) !!},
 					color: "#E9967A",
-					highlight: "#616774",
+					highlight: "#E9967A",
 					label: "Beni" //DarkSalmon
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["RIBERALTA"]) !!},
 					color: "#8FBC8F",
-					highlight: "#616774",
+					highlight: "#94BD94",
 					label: "Riberalta" //DarkSeaGreen
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["GUAYARAMERIN"]) !!},
 					color: "#F4A460",
-					highlight: "#616774",
+					highlight: "#F6A965",
 					label: "Guayaramerin" //SandyBrown
 				},
 				{
 					value: {!! json_encode($list_affiliateByDisctrict["PANDO"]) !!},
 					color: "#4682B4",
-					highlight: "#616774",
+					highlight: "#528AB7",
 					label: "Pando" //SteelBlue
 				}
 
 			];
-
 
 	var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
 	var barAporteVoluntario = {
