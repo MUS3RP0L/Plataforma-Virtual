d62a41121b2bd5f45ecb45f16e505635a556ac99
erickguis <aleguis@icloud.com>
2016-07-25 16:07:02 -0400

    updated breakdown

--- database/seeds/BreakdownTableSeeder.php
+++ database/seeds/BreakdownTableSeeder.php
@@ -22,24 +22,26 @@ class BreakdownTableSeeder extends Seeder
     private function createBreakdowns()
     {
         $statuses = [
-            
+
             ['code' => '1', 'name' => 'Disponibilidad'],// Servicio Disponibilidad *
-            ['code' => '2', 'name' => 'Dir. Nal. Pofoma'],//Servicio 
-            ['code' => '3', 'name' => 'Item Cero'],//Comisión * 
+            ['code' => '2', 'name' => 'Dir. Nal. Pofoma'],//Servicio
+            ['code' => '3', 'name' => 'Item Cero'],//Comisión *
+            ['code' => '4', 'name' => 'S/N'],//
             ['code' => '5', 'name' => 'Bat. Seg. Física Privada'],//Servicio Batallón *
             ['code' => '6', 'name' => 'Juzgados Policiales'],//Servicio
+            ['code' => '7', 'name' => 'S/N']//
             ['code' => '8', 'name' => 'Escuadrón de Seg. Los Pumas'],//Servicio
             ['code' => '9', 'name' => 'Dir. Nal. Seg. Penitenciaria'],//Servicio
             ['code' => '0', 'name' => 'Servicio'],//Servicio
-            ['code' => '4', 'name' => 'S/N'],//
-            ['code' => '7', 'name' => 'S/N']//
+
+
 
         ];
 
         foreach ($statuses as $status) {
 
             Muserpol\Breakdown::create($status);
-            
+
         }
     }
 }
