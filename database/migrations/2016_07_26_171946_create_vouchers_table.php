b3f02b53bcb2d7475ce4c3d299dad2178e2d49c0
erickguis <aleguis@icloud.com>
2016-07-26 18:08:29 -0400

    created table voucher

---
+++ database/migrations/2016_07_26_171946_create_vouchers_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('voucher_types', function(Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+
+        });
+
+        Schema::create('vouchers', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->UnsignedBigInteger('user_id');
+            $table->UnsignedBigInteger('affiliate_id');
+            $table->UnsignedBigInteger('voucher_type_id');
+            $table->UnsignedBigInteger('contribution_type_id')->nullable();
+            $table->string('code');
+            $table->string('concept');
+            $table->increments('total');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('affiliate_id')->references('id')->on('affiliates');
+            $table->foreign('voucher_type_id')->references('id')->on('voucher_type');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('voucher_types');
+        Schema::drop('vouchers');
+    }
+}
