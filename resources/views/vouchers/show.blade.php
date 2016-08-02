e511dbbe7f603ee89ed35fd12aab6f47b99de972
erickguis <aleguis@icloud.com>
2016-08-02 11:48:22 -0400

    working on view voucher

---
+++ resources/views/vouchers/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+
+@section('content')
+<div class="container-fluid">
+    {!! Breadcrumbs::render('show_direct_contribution') !!}
+    <div class="row">
+        <div class="col-md-12 text-right">
+            <a href="{!! url('direct_contribution') !!}" style="margin:-6px 1px 12px;" class="btn btn-raised btn-warning" data-toggle="tooltip" data-placement="top" data-original-title="Atrás">
+                &nbsp;&nbsp;<span class="glyphicon glyphicon-share-alt"></span>&nbsp;&nbsp;
+            </a>
+        </div>
+    </div>
+
+    @include('direct_contributions.simple_info')
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Despliegue</h3>
+                </div>
+                <div class="panel-body">
+                    <iframe src="{!! url('print_direct_contribution/' . $direct_contribution->id) !!}" width="99%" height="1200"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@push('scripts')
+<script>
+
+</script>
+@endpush
