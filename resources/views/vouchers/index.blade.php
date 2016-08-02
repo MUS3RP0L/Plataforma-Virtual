b6a9a9922259b4b8db757687af21f25758cccfb9
erickguis <aleguis@icloud.com>
2016-08-02 11:47:30 -0400

    working on show index vouchers

--- resources/views/vouchers/index.blade.php
+++ resources/views/vouchers/index.blade.php
@@ -12,15 +12,65 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-12">
-                            <table class="table table-striped table-hover" id="aportes_pago-table">
+                            <form method="POST" id="search-form" role="form">
+                                <div class="row"><br>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            {!! Form::label('code', 'Número Trámite', ['class' => 'col-md-5 control-label']) !!}
+                                            <div class="col-md-7">
+                                                {!! Form::text('code', '', ['class'=> 'form-control']) !!}
+                                                <span class="help-block">Escriba el Número Trámite</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            {!! Form::label('affiliate_name', 'Nombre Afiliado', ['class' => 'col-md-5 control-label']) !!}
+                                            <div class="col-md-7">
+                                                {!! Form::text('affiliate_name', '', ['class'=> 'form-control']) !!}
+                                                <span class="help-block">Escriba el Nombre Afiliado</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                                {!! Form::label('date', 'Fecha de Emsión', ['class' => 'col-md-5 control-label']) !!}
+                                            <div class="col-md-7">
+                                    			<div class="input-group">
+                                                    <input type="text" class="form-control datepicker" name="date" value="">
+                                                    <div class="input-group-addon">
+                                                        <span class="glyphicon glyphicon-calendar"></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row text-center">
+                                    <div class="form-group">
+                                        <div class="col-md-12">
+                                            <button type="reset" class="btn btn-raised btn-warning" data-toggle="tooltip" data-placement="bottom" data-original-title="Limpiar">&nbsp;<span class="glyphicon glyphicon-erase"></span>&nbsp;</button>
+                                            &nbsp;&nbsp;<button type="submit" class="btn btn-raised btn-success" data-toggle="tooltip" data-placement="bottom" data-original-title="Buscar">&nbsp;<span class="glyphicon glyphicon-search"></span>&nbsp;</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table class="table table-striped table-hover" id="vouchers-table">
                                 <thead>
                                     <tr class="success">
                                         <th class="text-center"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Número de Trámite">Número</div></th>
                                         <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Nombre de Afiliado">Nombre de Afiliado</div></th>
+                                        <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Concepto de Cobro">Concepto</div></th>
                                         <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Total a Pagar">Total a Pagar</div></th>
                                         <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Total a Pagar">Fecha Emisión</div></th>
-                                        {{-- <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Estado">Estado</div></th> --}}
-                                        {{-- <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Fecha de Pago">Fecha de Pago</div></th> --}}
+                                        <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Estado">Estado</div></th>
+                                        <th class="text-left"><div data-toggle="tooltip" data-placement="top" data-container="body" data-original-title="Fecha de Pago">Fecha de Pago</div></th>
                                         <th class="text-center">Acción</th>
                                     </tr>
                                 </thead>
@@ -36,23 +86,46 @@
 
 @push('scripts')
 <script>
-$(function() {
-    $('#aportes_pago-table').DataTable({
-        dom: '<"top">t<"bottom"p>',
+
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        language: "es",
+        orientation: "bottom right",
+        daysOfWeekDisabled: "0,6",
+        autoclose: true
+    });
+
+    var oTable = $('#vouchers-table').DataTable({
+        "dom": '<"top">t<"bottom"p>',
         processing: true,
         serverSide: true,
-        pageLength: 10,
-        ajax: '{!! route('get_voucher') !!}',
+        pageLength: 8,
         order: [0, "desc"],
+        ajax: {
+            url: '{!! route('get_voucher') !!}',
+            data: function (d) {
+                d.code = $('input[name=code]').val();
+                d.affiliate_name = $('input[name=affiliate_name]').val();
+                d.date = $('input[name=date]').val();
+                d.post = $('input[name=post]').val();
+            }
+        },
         columns: [
             { data: 'code', sClass: "text-center" },
             { data: 'affiliate_name', bSortable: false },
+            { data: 'voucher_type', bSortable: false },
             { data: 'total', bSortable: false },
             { data: 'created_at', bSortable: false },
-            // { data: 'status', bSortable: false },
+            { data: 'status', bSortable: false },
+            { data: 'payment_date', bSortable: false },
             { data: 'action', name: 'action', orderable: false, searchable: false, bSortable: false, sClass: "text-center" }
         ]
     });
-});
+
+    $('#search-form').on('submit', function(e) {
+        oTable.draw();
+        e.preventDefault();
+    });
+
 </script>
 @endpush
