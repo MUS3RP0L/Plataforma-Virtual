05cf8408d5223c674cb00df9a5d6f4f48e695299
felixbvf <felixbvf@gmail.com>
2016-08-08 11:08:52 -0400

    upgrade Util

--- app/Helper/Util.php
+++ app/Helper/Util.php
@@ -131,12 +131,13 @@ class Util
 	}
 
 	public static function decimal($number)
-	{   $number = str_replace ( ',' , '' , $number );
+	{ if ($number) {
+		$number = str_replace ( ',' , '' , $number );
 		$sueMon = substr($number, 0, -2);
 		$sueDeci = substr($number, -2);
 		$sue = $sueMon . "." . $sueDeci;
-
 		return $sue;
+	  }
 	}
 
 	public static function date($date)
