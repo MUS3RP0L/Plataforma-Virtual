162373442be9ef55c12f7e7f4ea176fc25ce7aa2
erickguis <aleguis@icloud.com>
2016-08-09 18:05:20 -0400

    bug fixes

--- resources/views/direct_contributions/calculation.blade.php
+++ resources/views/direct_contributions/calculation.blade.php
@@ -18,7 +18,77 @@
                 </div>
             </div>
 
-            @include('direct_contributions.simple_info')
+            <div class="row">
+
+                @include('affiliates.simple_info')
+
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3 class="panel-title"><span class="glyphicon glyphicon-list-alt"></span> Información de Aporte</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel-body" style="font-size: 14px">
+                            <div class="row">
+                                <div class="col-md-6">
+
+                                    <table class="table table-responsive" style="width:100%;">
+                                        <tr>
+                                            <td style="border-top:1px solid #d4e4cd;">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        Gestión
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                         {!! $year !!}
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="border-top:1px solid #d4e4cd;border-bottom:1px solid #d4e4cd;">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        Tipo Aporte
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        {!! $type !!}
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="col-md-6">
+                                    <table class="table" style="width:100%;">
+                                        <tr>
+                                            <td style="border-top:1px solid #d4e4cd;border-bottom:1px solid #d4e4cd;">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        Total
+                                                    </div>
+                                                    <div class="col-md-6">
+
+                                                        @if($direct_contribution)
+                                                            {!! $direct_contribution->total !!}
+                                                        @else
+                                                            <span data-bind="text: sum_total()">
+                                                        @endif
+
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="panel panel-primary">
                 <div class="panel-heading">
