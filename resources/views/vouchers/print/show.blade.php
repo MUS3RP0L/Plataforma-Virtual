4c98e852d0d4e9bfbc6eb174e764270d55cf0e9f
felixbvf <felixbvf@gmail.com>
2016-08-12 10:19:17 -0400

    upgrade View print voucher

--- resources/views/vouchers/print/show.blade.php
+++ resources/views/vouchers/print/show.blade.php
@@ -16,24 +16,22 @@
 <div id="project">
     <table>
         <tr>
-          <td colspan="4" class="grand service" style="text-align:center;">Periodo: {{ $direct_contribution->period() }}  </td>
+          <td colspan="3" class="grand service" style="text-align:center;">Periodo: {{ $direct_contribution->period() }}  </td>
         </tr>
       <tr>
         <td class="grand service" style="text-align:center;" >Total a Pagar</td>
-        <td class="grand service" style="text-align:center;">Literal</td>
         <td class="grand service" style="text-align:center;">Concepto</td>
         <td class="grand service" style="text-align:center;">Forma de Pago</td>
 
       </tr>
       <tr>
         <th class="info" style="text-align:center;" >Bs. {{ $voucher->total }}</th>
-        <th class="info" style="text-align:center;">{{ $voucher->total }}</th>
         <th class="info" style="text-align:center;">Aporte Voluntario Auxilio Mortuorio</th>
         <th class="info" style="text-align:center;"> Efectivo </th>
 
       </tr>
-
     </table>
+    <h4 style="text-align:left;"> SON: {{ Util::convertir($voucher->total,'Bolivianos','Centavos') }}.</h4>
     <br>
     <br>
     <br>
